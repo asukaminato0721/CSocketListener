f1a9b1cc28f50243baccf1a78d11eba382944e3f
JerryI <krikus.ms@gmail.com>
2023-09-29 10:23:54 +0200

    backup

--- LibraryResources/Windows-x86-64/Working-pc68ep5-6968-12440-1/csockets.bat
+++
@@ -1,3 +0,0 @@
-set PATH=C:\mingw64\bin\;%PATH%
-
-"C:\mingw64\bin\gcc.exe" -shared -o "C:\Users\vasinkir\Desktop\CSocketListener\LibraryResources\Windows-x86-64\Working-pc68ep5-6968-12440-1\csockets.dll" -g  -I"C:\Program Files\Wolfram Research\Wolfram Engine\13.0\SystemFiles\IncludeFiles\C" -I"C:\Program Files\Wolfram Research\Wolfram Engine\13.0\SystemFiles\Links\MathLink\DeveloperKit\Windows-x86-64\CompilerAdditions" "C:\Users\vasinkir\Desktop\CSocketListener\Source\csockets.c"  -L"C:\Program Files\Wolfram Research\Wolfram Engine\13.0\SystemFiles\Links\MathLink\DeveloperKit\Windows-x86-64\CompilerAdditions" -L"C:\Program Files\Wolfram Research\Wolfram Engine\13.0\SystemFiles\Libraries\Windows-x86-64"  -l"ml64i4m" -l"ws2_32" 2>&1
